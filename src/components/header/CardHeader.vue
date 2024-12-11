429b1c6f638edb9ed99a1e356e4512ec59482d53
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2024-12-11 18:21:54 -0300

    feat: create header and sidebar component

---
+++ src/components/header/CardHeader.vue
@@ -0,0 +1,98 @@
+<template>
+    <header>
+        <div class="logo">
+            <h2>Jabill Notes</h2>
+        </div>
+        <div v-if="isLogged" class="perfil">
+            <div class="userProfile">
+                <p>Guilherme</p>
+                <img :src="perfilImage" alt="perfil image">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "CardHeader",
+    props: {
+        isLogged :{
+            type: Boolean,
+            require: true
+        },
+        perfilImage: {
+            type: String,
+            default: () => require('@/assets/perfil.jpeg'),
+        }
+    }
+}
+</script>
+
+<style>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+    width: 100%;
+    border-bottom: solid 1px black;
+}
+
+
+.logo{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 15%;
+    height: 100%;
+    background-color: gray;
+    color: white
+}
+
+.perfil{
+    margin-left: 30px;
+}
+
+.userProfile{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+}
+
+.userProfile img{
+    border-radius: 50%;
+    width: 60px;
+    
+}
+
+.userProfile img:hover{
+    cursor: pointer;
+    box-shadow: 0 0 15px black;
+    transition: 0.1s;
+}
+
+@media screen and (max-width: 900px) {
+    .logo{
+        width: 35%;
+    }
+}
+
+@media screen and (max-width: 400px) {
+    .logo{
+        width: 50%;
+    }
+
+    .logo h2{
+        font-size: 20px;
+    }
+
+    .userProfile img{
+        width: 45px;
+    }
+
+    .userProfile p{
+        font-size: 14px;
+    }
+}
+</style>
---
+++ src/components/sidebar/CardSidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="sidebar"></div>
+</template>
+
+<script>
+export default{
+    name: "CardSidebar"
+}
+</script>
+
+<style>
+    .sidebar{
+        height: 100vh;
+        background-color: black;
+        width: 15%;
+    }
+
+    @media screen and (max-width: 900px) {
+        .sidebar{
+            width: 35%;
+        }
+    }
+
+
+    @media screen and (max-width: 400px) {
+        .sidebar{
+            width: 50%;
+        }
+    }
+</style>
