eaf2cd436584cc8d0a8da414f8a97b95b5214bcb
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2024-12-27 08:58:21 -0300

    feat: create page title component

---
+++ src/components/pageTitle/CardPageTitle.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="title">
+        <span class="emoji">{{ String.fromCodePoint(0x1F4C3) }}</span>
+        <h1 contenteditable="true" @input="updateTitle"> {{ title }}</h1>
+    </div>
+</template>
+
+<script>
+import "./CardPageTitle.css";
+export default{
+    name: "CardPageTitle",
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        emoji: {
+            type: String,
+        }
+    }, 
+    methods: {
+        updateTitle(event){
+            this.$emit("title-change", event.target.innerText)
+        }
+    }
+}
+</script>
