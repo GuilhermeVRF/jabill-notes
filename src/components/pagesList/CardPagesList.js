02ecf1debf178757f3fb9da79422599ca7972252
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-01-30 19:33:02 -0300

    feat: allow the user to delete a page

--- src/components/pagesList/CardPagesList.js
+++ src/components/pagesList/CardPagesList.js
@@ -14,3 +14,19 @@ export async function createPageInServer(title){
         return null
     }    
 }
+
+export async function deletePage(slug){
+    try{
+        const response = await fetch(`http://localhost:5000/page/${slug}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("user_token")}`
+            }
+        })
+
+        const responseBody = await response.json();
+        window.showPopup(responseBody.status, responseBody.message);
+    }catch(exception){
+        return null;
+    }
+}
--- src/components/pagesList/CardPagesList.vue
+++ src/components/pagesList/CardPagesList.vue
@@ -5,15 +5,21 @@
             <input  type="button" value="+" class="btn btn-success" @click="createPage">
         </form>
         <ul :class="$style.pagesList">
-            <li v-for="([slug, page]) of Object.entries(pages)" :key="page.id">
-                <RouterLink :to="'/content/' + slug">{{ String.fromCodePoint(0x1F4C3) }} {{ page.title }}</RouterLink>
+            <li v-for="([slug, page]) of Object.entries(pages)" :key="page.id" :class="$style.pageLink">
+                <div :class="$style.pageLinkTitle">
+                    <span>{{ String.fromCodePoint(0x1F4C3) }}</span>
+                    <RouterLink :to="'/content/' + slug"> {{ page.title }}</RouterLink>
+                </div>
+                <button :class="$style.deletePage" @click=deletePage(slug)>
+                    <img src="@/assets/trash.png" alt="X">
+                </button>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-import { createPageInServer } from "./CardPagesList.js";
+import { createPageInServer, deletePage } from "./CardPagesList.js";
 import style from "./CardPagesList.module.css"
 export default{
     name: "CardPagesList",
@@ -38,6 +44,10 @@ export default{
             const page = await createPageInServer(this.newPageTitle);
 
             if(page) this.$emit("create-page", page)
+        },
+        deletePage(slug) {
+            delete this.pages[slug];
+            deletePage(slug)
         }
     },
     computed: {
