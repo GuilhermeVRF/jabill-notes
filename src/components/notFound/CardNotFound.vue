4ea80bc0d16aaa81696da799ecd015d054a2044c
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-02-09 18:29:04 -0300

    feat: add not found component when a page doesnt exists

---
+++ src/components/notFound/CardNotFound.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :class="$style.notFound">
+        <CardLoggedHeader />
+        <div :class="$style.notFoundContainer">
+            <img src="@/assets/notFound.png" >
+            <p>A página solicita não foi encontrada!</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import CardLoggedHeader from "../loggedHeader/CardLoggedHeader.vue";
+import styles from "./CardNotFound.module.css"
+export default{
+    name: "CardNotFound",
+    components: {
+        CardLoggedHeader
+    },
+    computed: {
+    $style(){
+        return styles;
+    }
+  }
+}
+</script>
